e3c92a2b13e16e3dd04f34ece6203adc3322d5ae
bmaloy19 <bmaloy19@gmail.com>
2025-09-26 21:15:22 -0500

    first commit

---
+++ BlazorApp2/Data/Models/Part.cs
@@ -0,0 +1,9 @@
+namespace BlazorApp2.Data.Models;
+
+public class Part
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Cost { get; set; }
+    public int Quantity { get; set; }
+}
