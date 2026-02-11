614e1ae6268daabbafaa0d333e447f917be624d4
Bryce <bmaloy19@gmail.com>
2026-02-10 22:04:55 -0600

    wip email service
    need to figure out goggle stmp

--- BlazorApp2/Program.cs
+++ BlazorApp2/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using BlazorApp2.Components;
 using BlazorApp2.Components.Account;
@@ -42,7 +43,21 @@ builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.Requ
     .AddSignInManager()
     .AddDefaultTokenProviders();
 
-builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+// Configure Email Settings
+builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection("EmailSettings"));
+
+// Register email services based on environment
+if (builder.Environment.IsDevelopment())
+{
+    // In development, use the development email sender that saves emails to files
+    builder.Services.AddSingleton<IEmailSender, DevelopmentEmailSender>();
+}
+else
+{
+    // In production, use SMTP email sender
+    builder.Services.AddSingleton<IEmailSender, SmtpEmailSender>();
+}
+builder.Services.AddSingleton<IEmailSender<ApplicationUser>, EmailSenderService>();
 
 // Register application services
 builder.Services.AddScoped<VehicleService>();
